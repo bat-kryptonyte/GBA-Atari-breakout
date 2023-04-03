992840557b0acae7d3368a7b39ec6f743360a8c9
cdeng65 <cdeng65@gatech.edu>
2023-04-02 23:11:34 -0400

    lint and format

--- gba.c
+++ gba.c
@@ -1,6 +1,6 @@
 #include "gba.h"
 
-volatile unsigned short *videoBuffer = (volatile unsigned short *) 0x6000000;
+volatile unsigned short * videoBuffer = (volatile unsigned short * ) 0x6000000;
 u32 vBlankCounter = 0;
 
 /*
@@ -8,37 +8,39 @@ u32 vBlankCounter = 0;
   Completing this function is required.
 */
 void waitForVBlank(void) {
-  // TODO: IMPLEMENT
+    // TODO: IMPLEMENT
 
-  // (1)
-  // Write a while loop that loops until we're NOT in vBlank anymore:
-  // (This prevents counting one VBlank more than once if your app is too fast)
-  while(SCANLINECOUNTER > 160);
+    // (1)
+    // Write a while loop that loops until we're NOT in vBlank anymore:
+    // (This prevents counting one VBlank more than once if your app is too fast)
+    while (SCANLINECOUNTER > 160);
 
-  // (2)
-  // Write a while loop that keeps going until we're in vBlank:
-  while(SCANLINECOUNTER < 160);
+    // (2)
+    // Write a while loop that keeps going until we're in vBlank:
+    while (SCANLINECOUNTER < 160);
 
-  // (3)
-  // Finally, increment the vBlank counter:
-  vBlankCounter++;
+    // (3)
+    // Finally, increment the vBlank counter:
+    vBlankCounter++;
 }
 
 static int __qran_seed = 42;
 static int qran(void) {
-  __qran_seed = 1664525 * __qran_seed + 1013904223;
-  return (__qran_seed >> 16) & 0x7FFF;
+    __qran_seed = 1664525 * __qran_seed + 1013904223;
+    return (__qran_seed >> 16) & 0x7FFF;
 }
 
-int randint(int min, int max) { return (qran() * (max - min) >> 15) + min; }
+int randint(int min, int max) {
+    return (qran() * (max - min) >> 15) + min;
+}
 
 /*
   Sets a pixel in the video buffer to a given color.
   Using DMA is NOT recommended. (In fact, using DMA with this function would be really slow!)
 */
 void setPixel(int row, int col, u16 color) {
-  // TODO: IMPLEMENT
-  videoBuffer[OFFSET(row, col, WIDTH)] = color;
+    // TODO: IMPLEMENT
+    videoBuffer[OFFSET(row, col, WIDTH)] = color;
 }
 
 /*
@@ -47,12 +49,12 @@ void setPixel(int row, int col, u16 color) {
   This function can be completed using `height` DMA calls. 
 */
 void drawRectDMA(int row, int col, int width, int height, volatile u16 color) {
-  // TODO: IMPLEMENT
-  for (int r = 0; r < height; r++) {
-    DMA[3].src = &color;
-    DMA[3].dst = &videoBuffer[OFFSET(row + r, col, WIDTH)];
-    DMA[3].cnt = width | DMA_ON | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT;
-  }
+    // TODO: IMPLEMENT
+    for (int r = 0; r < height; r++) {
+        DMA[3].src = & color;
+        DMA[3].dst = & videoBuffer[OFFSET(row + r, col, WIDTH)];
+        DMA[3].cnt = width | DMA_ON | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT;
+    }
 }
 
 /*
@@ -60,9 +62,9 @@ void drawRectDMA(int row, int col, int width, int height, volatile u16 color) {
   The image passed in must be of size WIDTH * HEIGHT.
   This function can be completed using a single DMA call.
 */
-void drawFullScreenImageDMA(const u16 *image) {
-  // TODO: IMPLEMENT
-  drawImageDMA(0,0,WIDTH,HEIGHT, image);
+void drawFullScreenImageDMA(const u16 * image) {
+    // TODO: IMPLEMENT
+    drawImageDMA(0, 0, WIDTH, HEIGHT, image);
 }
 
 /*
@@ -72,13 +74,14 @@ void drawFullScreenImageDMA(const u16 *image) {
   Completing this function is required.
   This function can be completed using `height` DMA calls. Solutions that use more DMA calls will not get credit.
 */
-void drawImageDMA(int row, int col, int width, int height, const u16 *image) {
-  // TODO: IMPLEMENT
-  for (int r = 0; r < height; r++) {
-    DMA[3].src = &image[OFFSET(r, 0, width)];
-    DMA[3].dst = &videoBuffer[OFFSET(row + r, col, 240)];
-    DMA[3].cnt = width | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_INCREMENT;
-  }
+void drawImageDMA(int row, int col, int width, int height,
+    const u16 * image) {
+    // TODO: IMPLEMENT
+    for (int r = 0; r < height; r++) {
+        DMA[3].src = & image[OFFSET(r, 0, width)];
+        DMA[3].dst = & videoBuffer[OFFSET(row + r, col, 240)];
+        DMA[3].cnt = width | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_INCREMENT;
+    }
 }
 
 /*
@@ -87,12 +90,13 @@ void drawImageDMA(int row, int col, int width, int height, const u16 *image) {
   The image passed in must be of size WIDTH * HEIGHT.
   This function can be completed using `height` DMA calls.
 */
-void undrawImageDMA(int row, int col, int width, int height, const u16 *image) {
-  for (int r = 0; r < height; r++) {
-    DMA[3].src = &image[OFFSET(row + r, col, WIDTH)];
-    DMA[3].dst = &videoBuffer[OFFSET(row + r, col, WIDTH)];
-    DMA[3].cnt = width | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_INCREMENT;
-  }
+void undrawImageDMA(int row, int col, int width, int height,
+    const u16 * image) {
+    for (int r = 0; r < height; r++) {
+        DMA[3].src = & image[OFFSET(row + r, col, WIDTH)];
+        DMA[3].dst = & videoBuffer[OFFSET(row + r, col, WIDTH)];
+        DMA[3].cnt = width | DMA_ON | DMA_SOURCE_INCREMENT | DMA_DESTINATION_INCREMENT;
+    }
 }
 
 /*
@@ -100,50 +104,50 @@ void undrawImageDMA(int row, int col, int width, int height, const u16 *image) {
   This function can be completed using a single DMA call.
 */
 void fillScreenDMA(volatile u16 color) {
-  for (int r = 0; r < HEIGHT; r++) {
-    DMA[3].src = &color;
-    DMA[3].dst = &videoBuffer[OFFSET(r, 0, WIDTH)];
-    DMA[3].cnt = WIDTH | DMA_ON | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT;
-  }
+    for (int r = 0; r < HEIGHT; r++) {
+        DMA[3].src = & color;
+        DMA[3].dst = & videoBuffer[OFFSET(r, 0, WIDTH)];
+        DMA[3].cnt = WIDTH | DMA_ON | DMA_SOURCE_FIXED | DMA_DESTINATION_INCREMENT;
+    }
 }
 
 /* STRING-DRAWING FUNCTIONS (provided) */
 void drawChar(int row, int col, char ch, u16 color) {
-  for (int i = 0; i < 6; i++) {
-    for (int j = 0; j < 8; j++) {
-      if (fontdata_6x8[OFFSET(j, i, 6) + ch * 48]) {
-        setPixel(row + j, col + i, color);
-      }
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 8; j++) {
+            if (fontdata_6x8[OFFSET(j, i, 6) + ch * 48]) {
+                setPixel(row + j, col + i, color);
+            }
+        }
     }
-  }
 }
 
-void drawString(int row, int col, char *str, u16 color) {
-  while (*str) {
-    drawChar(row, col, *str++, color);
-    col += 6;
-  }
+void drawString(int row, int col, char * str, u16 color) {
+    while ( * str) {
+        drawChar(row, col, * str++, color);
+        col += 6;
+    }
 }
 
-void drawCenteredString(int row, int col, int width, int height, char *str, u16 color) {
-  u32 len = 0;
-  char *strCpy = str;
-  while (*strCpy) {
-    len++;
-    strCpy++;
-  }
+void drawCenteredString(int row, int col, int width, int height, char * str, u16 color) {
+    u32 len = 0;
+    char * strCpy = str;
+    while ( * strCpy) {
+        len++;
+        strCpy++;
+    }
 
-  u32 strWidth = 6 * len;
-  u32 strHeight = 8;
+    u32 strWidth = 6 * len;
+    u32 strHeight = 8;
 
-  int new_row = row + ((height - strHeight) >> 1);
-  int new_col = col + ((width - strWidth) >> 1);
-  drawString(new_row, new_col, str, color);
+    int new_row = row + ((height - strHeight) >> 1);
+    int new_col = col + ((width - strWidth) >> 1);
+    drawString(new_row, new_col, str, color);
 }
 
 void delay(int n) {
-// delay for n tenths of a second
-  volatile int x = 0;
-  for (int i=0; i<n*8000; i++)
-  x++;
+    // delay for n tenths of a second
+    volatile int x = 0;
+    for (int i = 0; i < n * 8000; i++)
+        x++;
 }
--- main.h
+++ main.h
@@ -6,77 +6,77 @@
 // TODO: Create any necessary structs
 
 /*
-* For example, for a Snake game, one could be:
-*
-* struct snake {
-*   int heading;
-*   int length;
-*   int row;
-*   int col;
-* };
-*
-* Example of a struct to hold state machine data:
-*
-* struct state {
-*   int currentState;
-*   int nextState;
-* };
-*
-*/
+ * For example, for a Snake game, one could be:
+ *
+ * struct snake {
+ *   int heading;
+ *   int length;
+ *   int row;
+ *   int col;
+ * };
+ *
+ * Example of a struct to hold state machine data:
+ *
+ * struct state {
+ *   int currentState;
+ *   int nextState;
+ * };
+ *
+ */
 #define NUM_TILES 33
 
 enum gba_state {
-  START, 
-  PLAY,
-  WIN,
-  LOSE,
+    START,
+    PLAY,
+    WIN,
+    LOSE,
 };
 
 enum gamestate {
-  GS_INITBOUNCE,
-  GS_BOUNCE
+    GS_INITBOUNCE,
+    GS_BOUNCE
 };
 
 struct ball {
-          int row;
-          int col;
-          int rd;
-          int cd;
-          unsigned short color;
-  };
+    int row;
+    int col;
+    int rd;
+    int cd;
+    unsigned short color;
+};
 
-  struct player {
-          int row;
-          int col;
-          int rd;
-          int cd;
-          const u16 *image;
-          
-  };
+struct player {
+    int row;
+    int col;
+    int rd;
+    int cd;
+    const u16 * image;
 
-  struct g_target {
-          int row;
-          int col;
-          const u16 *image;
-          int width;
-          int height;
-          int hit;
-  };
+};
 
-  struct state {
+struct g_target {
+    int row;
+    int col;
+    const u16 * image;
+    int width;
+    int height;
+    int hit;
+};
+
+struct state {
     enum gamestate gamestate;
     int size;
     struct ball balls;
-  };
+};
 
-  struct player_state {
+struct player_state {
     int width;
     int height;
     struct player players;
-  };
+};
 
-  struct target_state {
+struct target_state {
     struct g_target targets[NUM_TILES];
-  };
+};
 
 #endif
